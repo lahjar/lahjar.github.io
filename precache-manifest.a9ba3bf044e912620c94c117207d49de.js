36ffc4d6d26a9d01d94b351929825a04dd4bf828
lahjar <rishal.raj.v@gmail.com>
2020-02-22 21:55:15 +0530

    Updates

---
+++ precache-manifest.a9ba3bf044e912620c94c117207d49de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a264325a665edfa39c3d5a589a50cdfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fdc91a9b25812282e1d",
+    "url": "/static/css/main.5c9e1317.chunk.css"
+  },
+  {
+    "revision": "bfd74e79ba4df8c83439",
+    "url": "/static/js/2.653982ad.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "/static/js/2.653982ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fdc91a9b25812282e1d",
+    "url": "/static/js/main.6dd5c72a.chunk.js"
+  },
+  {
+    "revision": "7910ebda413ba1a55c72",
+    "url": "/static/js/runtime-main.24b84f26.js"
+  },
+  {
+    "revision": "a578341502de37ffb822093a77dea5ce",
+    "url": "/static/media/back.a5783415.svg"
+  }
+]);
